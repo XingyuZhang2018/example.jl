d0c3fa693a4109d1cd5007fb261df4da78e3734c
Documenter.jl <documenter@juliadocs.github.io>
2021-09-27 09:32:15 +0000

    build based on c089753

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#example.jl-Documentation","page":"Home","title":"example.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = example","category":"page"},{"location":"","page":"Home","title":"Home","text":"func(x)","category":"page"},{"location":"#example.func-Tuple{Any}","page":"Home","title":"example.func","text":"func(x)\n\nReturns double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+}
