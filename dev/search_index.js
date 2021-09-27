9156469069f1c3c0cea41882b140698d351b6003
Documenter.jl <documenter@juliadocs.github.io>
2021-09-27 09:42:54 +0000

    build based on f22bc84

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#example.jl-Documentation","page":"Home","title":"example.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = example","category":"page"},{"location":"","page":"Home","title":"Home","text":"func(x)","category":"page"},{"location":"#example.func-Tuple{Any}","page":"Home","title":"example.func","text":"func(x)\n\nReturns double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+[{"location":"#example.jl","page":"Home","title":"example.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"func(x)","category":"page"},{"location":"#example.func-Tuple{Any}","page":"Home","title":"example.func","text":"func(x)\n\nReturns double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
 }
